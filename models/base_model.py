731bd8b70a0903b88444d83194c6326354f2ac07
Abdifatah Nur <deenynur21@gmail.com>
2022-08-08 00:54:48 +0300

    t

--- models/base_model.py
+++ models/base_model.py
@@ -38,7 +38,7 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.utcnow()
             self.updated_at = datetime.utcnow()
-            models.storage.new(self)
+            #models.storage.new(self)
         else:
             for key, value in kwargs.items():
                 if key in ("updated_at", "created_at"):
@@ -62,7 +62,7 @@ class BaseModel:
         'updated_at' - with the current datetime
         """
         self.updated_at = datetime.utcnow()
-        models.storage.save()
+        #models.storage.save()
 
     def to_dict(self):
         """
