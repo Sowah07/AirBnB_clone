199d1af70fb37e5853d9939c61333240b68f16bc
Abdifatah Nur <deenynur21@gmail.com>
2022-08-08 02:21:42 +0300

    task 5

---
+++ models/user.py
@@ -0,0 +1,13 @@
+#!usr/bin/python3
+""" State class """
+
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """Creates a new user"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
